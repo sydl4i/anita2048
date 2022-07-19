d6c73eab6e618edfc1c0be033f2b1e3cc7ee3a65
Sydney Lai <SydneyL22@lakesideschool.org>
2022-07-19 10:57:15 -0700

    7/18 2

--- script.js
+++ script.js
@@ -39,9 +39,43 @@ document.addEventListener('DOMContentLoaded', () => {
                 let row = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)]
     
                 console.log(row)
+
+                let filteredRow = row.filter(num => num)
+                let missing = 4 - filteredRow.length
+                let zeros = Array(missing).fill(0)
+                let newRow = zeros.concat(filteredRow)
+
+                squares[i].innerHTML = newRow[0]
+                squares[i+1].innerHTML = newRow[1]
+                squares[i+2].innerHTML = newRow[2]
+                squares[i+3].innerHTML = newRow[3]
             }
         }
     }
 
-    moveRight()
+    //swipe left
+    function moveLeft() {
+        for (let i=0; i<16; i++) {
+            if (i % 4 === 0) {
+                let totalOne = squares[i].innerHTML
+                let totalTwo = squares[i+1].innerHTML
+                let totalThree = squares[i+2].innerHTML
+                let totalFour = squares[i+3].innerHTML
+                let row = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)]
+    
+                console.log(row)
+
+                let filteredRow = row.filter(num => num)
+                let missing = 4 - filteredRow.length
+                let zeros = Array(missing).fill(0)
+                let newRow = filteredRow.concat(zeros)
+
+                squares[i].innerHTML = newRow[0]
+                squares[i+1].innerHTML = newRow[1]
+                squares[i+2].innerHTML = newRow[2]
+                squares[i+3].innerHTML = newRow[3]
+            }
+        }
+    }
+
 })
