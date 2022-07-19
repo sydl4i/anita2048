b74186d6b4bc4052f9c2fa1ce14836e1367a2444
Sydney Lai <SydneyL22@lakesideschool.org>
2022-07-19 10:20:47 -0700

    7/19

--- script.js
+++ script.js
@@ -13,14 +13,35 @@ document.addEventListener('DOMContentLoaded', () => {
             gridDisplay.appendChild(square)
             squares.push(square)
         }
+        generate()
+        generate()
+        
     }
 
     createBoard()
 
     //generate a number randomly
     function generate() {
-        randomNumber = Math.floor(Math.random() * squares.length)
-        
+        let randomNumber = Math.floor(Math.random() * squares.length)
+        if (squares[randomNumber].innerHTML == 0) {
+            squares[randomNumber].innerHTML = 2
+        } else generate()
     }
 
+    //swipe right
+    function moveRight() {
+        for (let i=0; i<16; i++) {
+            if (i % 4 === 0) {
+                let totalOne = squares[i].innerHTML
+                let totalTwo = squares[i+1].innerHTML
+                let totalThree = squares[i+2].innerHTML
+                let totalFour = squares[i+3].innerHTML
+                let row = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)]
+    
+                console.log(row)
+            }
+        }
+    }
+
+    moveRight()
 })
